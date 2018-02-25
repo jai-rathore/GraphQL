28bb1504c8882c8b0f08a1e9f1a3d7d3f006e339
Jai Rathore <jai@Jais-MacBook-Pro.local>
2018-02-25 09:34:08 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const schema = require('./schema/schema')
+const app = express();
+const expressGraphQL = require('express-graphql');
+
+app.use('/graphql', expressGraphQL({
+    schema,
+    graphiql:true
+}));
+
+app.listen(4000, ()=> {
+    console.log("listening");
+});
